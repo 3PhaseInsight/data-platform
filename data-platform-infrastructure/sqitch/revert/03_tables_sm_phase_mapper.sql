c6106dd7ce6e4c3e0bdb30f5ab98b70a7b789785
Tian <tian@inilab.dk>
2025-12-17 15:55:23 +0100

    feat(sqitch): add deploy, revert and verify files with scripts git status

---
+++ data-platform-infrastructure/sqitch/revert/03_tables_sm_phase_mapper.sql
@@ -0,0 +1,7 @@
+-- Revert 3phi-db:03_tables_sm_phase_mapper from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ data-platform-infrastructure/sqitch/verify/03_tables_sm_phase_mapper.sql
@@ -0,0 +1,7 @@
+-- Verify 3phi-db:03_tables_sm_phase_mapper on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
