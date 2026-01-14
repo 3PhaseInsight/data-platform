9d799d1d5b6b75cc37e13a47f24057da9f57634f
Christian Fotteler <chris@inilab.dk>
2026-01-14 10:08:28 +0100

    Make dag_id and run_id nullable

--- data-platform-infrastructure/sqitch/deploy/04_tables_result_label.sql
+++ data-platform-infrastructure/sqitch/deploy/04_tables_result_label.sql
@@ -10,8 +10,8 @@ CREATE TYPE public.result_phase AS ENUM (
 
 CREATE TABLE IF NOT EXISTS public.run_result (
     id              uuid PRIMARY KEY,
-    dag_id          text NOT NULL, -- part 1 of unique dag run identifier
-    run_id          text NOT NULL, -- part 2 of unique dag run identifier
+    dag_id          text, -- part 1 of unique dag run identifier, nullable for when not using airflow
+    run_id          text, -- part 2 of unique dag run identifier, nullable for when not using airflow
     meter_id        bigint NOT NULL REFERENCES public.meter(id),
     phase           result_phase NOT NULL,
     label_type      text NOT NULL,
