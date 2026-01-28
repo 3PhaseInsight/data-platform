637fc9e703b6c2efac39994a132b697cacf92e9e
Christian Fotteler <chris@inilab.dk>
2026-01-28 13:05:00 +0100

    Update test topology dag with new methods

--- dags/test_topology.py
+++ dags/test_topology.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 import threephi_framework.db.db as threephi_db
 from threephi_framework import TopologyController
+from threephi_framework.resources.topology.assets.meter import MeterResource
 from threephi_framework import DataApp
 
 
@@ -15,6 +16,9 @@ class TopologyTester(DataApp):
     def __init__(self, config):
         super().__init__(config)
         self.substation_id = config["substation_id"]
+        self.delivery_point_id = config["delivery_point_id"]
+        self.cabinet_id = config["cabinet_id"]
+        self.feeder_id = config["feeder_id"]
         self.topology_controller = TopologyController(threephi_db.new_session)
 
     def run(self):
@@ -23,8 +27,22 @@ class TopologyTester(DataApp):
         logging.info(f"Meters for substation {self.substation_id}: {meters}")
 
         # Test get_meters
-        meters2 = self.topology_controller.get_meters(True, True)
-        logging.info(f"get_meters(has_heat_pump=True, has_solar_panel=True): {meters2}")
+        meters = self.topology_controller.get_meters(True, True)
+        logging.info(f"get_meters(has_heat_pump=True, has_solar_panel=True): {meters}")
+
+
+        # Test get_meters_for_node (delivery_point)
+        meters = self.topology_controller.get_meters_for_node(self.delivery_point_id, "delivery_point")
+        logging.info(f"get_meters_for_node(node_id={self.delivery_point_id}, \"delivery_point\"): {meters}")
+
+        # Test get_meters_for_node (cabinet)
+        meters = self.topology_controller.get_meters_for_node(self.cabinet_id, "cabinet")
+        logging.info(f"get_meters_for_node(node_id={self.cabinet_id}, \"cabinet\"): {meters}")
+
+        # Test get_meters_for_node (feeder)
+        meters = self.topology_controller.get_meters_for_node(self.feeder_id, "lv_feeder")
+        logging.info(f"get_meters_for_node(node_id={self.feeder_id}, \"lv_feeder\"): {meters}")
+
 
 config_file = "test_topology.yaml"
 with open(
